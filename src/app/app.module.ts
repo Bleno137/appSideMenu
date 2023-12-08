ca7db8e479c242959b88ffa1ccb3e76ba40c53df
Bleno137 <bleno137@gmail.com>
2023-12-08 16:01:42 -0300

    sem node _modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,34 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIxgIl7d8bB4RqXbUrnN3P7DwhAnh7AEY",
+  authDomain: "pretocheiroso-6ad96.firebaseapp.com",
+  projectId: "pretocheiroso-6ad96",
+  storageBucket: "pretocheiroso-6ad96.appspot.com",
+  messagingSenderId: "894290509413",
+  appId: "1:894290509413:web:f986cbf18e6d1d3b93d08c"
+};
+
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}
